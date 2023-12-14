687678aa37e1ea205548533871007697806d9a85
Ilona Kny <ili@gonuts.cz>
2023-12-14 17:03:53 +0100

    search bar

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+
+  const onChangeSearch = query => setSearchQuery(query);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchBar}>
+        <Searchbar
+           placeholder="Search"
+           onChangeText={onChangeSearch}
+           value={searchQuery}
+        />
+      </View>
+      <View style={styles.list}>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
+    <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: 'white',
+    marginTop: StatusBar.currentHeight,
+    // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
+  searchBar: {
+    // flex: 0.05,
+    backgroundColor: 'green',
+    padding: 16,
+    // justifyContent: 'center',
+  },
+  list: {
+    flex: 1,
+    backgroundColor: 'blue',
+    padding: 16,
+  }
 });
